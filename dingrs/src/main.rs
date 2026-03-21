6467e3a3906dec985c400821fd7df3fbf552534f
Jakob Hautop <jakobhautop@gmail.com>
2026-03-21 12:27:44 +0100

    update ping

--- dingrs/src/main.rs
+++ dingrs/src/main.rs
@@ -1,3 +1,33 @@
+use std::io::{Read, Write};
+use std::net::TcpStream;
+
+const PORT: u16 = 6666;
+const IP: &str = "127.0.0.1";
+const NETUPSRV: &str = "{IP}:{PORT}";
+
+struct Client {
+    stream: TcpStream,
+}
+
+impl Client {
+    fn init() -> Self {
+        let mut stream = TcpStream::connect(NETUPSRV).unwrap();
+        return Self { stream };
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+    let command = &args[1];
+    let arg1 = &args[2];
+    let arg2 = &args[3];
+
+    match command.as_str() {
+        "ping" => {
+            println!("CLI: Beginning ping..");
+            let client = Client::init();
+            println!("CLI: Completed ping..");
+        }
+        _ => panic!("Unknown command: {command}"),
+    }
 }
