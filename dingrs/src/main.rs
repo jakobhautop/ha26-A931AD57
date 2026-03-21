388ec36d157c8840a7808d75ea585abc67b29271
Jakob Hautop <jakobhautop@gmail.com>
2026-03-21 12:49:31 +0100

    try say hi

--- dingrs/src/main.rs
+++ dingrs/src/main.rs
@@ -8,12 +8,24 @@ struct Client {
 }
 
 impl Client {
-    fn init() -> Self {
+    fn init() -> Client {
         println!("INI: Beginning client init");
         println!("INI: Netupsrc on {}", NETUPSRV);
         let mut stream = TcpStream::connect(NETUPSRV).unwrap();
         println!("INI: Completed client init");
         return Self { stream };
+    }
+
+    fn send_and_recv(&mut self, b: &[u8]) {
+        println!("SAR: Send {}", String::from_utf8_lossy(b));
+        self.stream.write_all(b).unwrap();
+        let mut buf = [0; 1024];
+        let n = self.stream.read(&mut buf).unwrap();
+        println!("SAR: Recv: {}", String::from_utf8_lossy(&buf[..n]));
+    }
+
+    fn ping(&mut self) {
+        self.send_and_recv(b"Hi");
     }
 }
 
